f22d8c0026b22f97f8c103c5813410a1631035cd
Anis5221 <parsonal494@gmail.com>
2022-02-15 15:27:47 +0600

    auth page added

---
+++ pages/auth/Login.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const Login = (props: Props) => {
+  return (
+    <div className="w-full h-screen">
+        <h1 className='text-center font-black '>hello login</h1>
+    </div>
+  )
+}
+
+export default Login
---
+++ pages/auth/Register.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Register = (props: Props) => {
+  return (
+    <div>register</div>
+  )
+}
+
+export default Register;
